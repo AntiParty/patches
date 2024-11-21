37544f98fa7acb2fbd96ac51fb07a1a688b943ed
Asuritys-Inc <75674175+AntiParty@users.noreply.github.com>
2024-11-21 15:13:17 -0700

    better formatting for commands

--- src/events/client/ClientReady.ts
+++ src/events/client/ClientReady.ts
@@ -1,6 +1,15 @@
 import { type DiscordClient } from "@/lib/client"
 import { Logger } from "@/lib/logger"
+import { ActivityType } from "discord.js"
+import { Client } from "discord-rpc"
+
+const rpc = new Client({ transport: "ipc" });
 
 export default async (client: DiscordClient) => {
   Logger.info(`Logged in as ${client.user?.tag}!`)
+
+  client.user?.setActivity({
+    name: "Santai.GG",
+    type: ActivityType.Watching,
+  })
 }
